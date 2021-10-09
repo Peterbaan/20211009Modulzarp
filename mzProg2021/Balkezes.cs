f844cab74eb1a7518e63511e2b98331fb518b028
VinczeP <vincze.peter.eger@gmail.com>
2021-10-09 20:36:00 +0200

    Első feladat kész

---
+++ mzProg2021/Balkezes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mzProg2021
+{
+    class Balkezes
+    {
+        public string nev { get; set; }
+        public int elso { get; set; }
+        public int utolso { get; set; }
+        public int suly { get; set; }
+        public int magassag { get; set; }
+
+        public Balkezes(string sor)
+        {
+            string[] s = sor.Split(';');
+            nev = s[0];
+            elso = int.Parse(s[1]);
+            utolso = int.Parse(s[2]);
+            suly = int.Parse(s[3]);
+            magassag = int.Parse(s[4]);
+        }
+        public Balkezes()
+        {
+        }
+    }
+
+}
---
+++ mzProg2021/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace mzProg2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("balkezesek.csv");
+            List<Balkezes> balkezesek = new List<Balkezes>();
+            foreach (string sor in sorok.Skip(1))
+            {
+                balkezesek.Add(new Balkezes());
+            }
+
+            int N = balkezesek.Count;//1. Feladat
+            Console.WriteLine($"{N} db balkezes játékos van.");
+
+            int Db1980Felett= 0;
+            for (int i = 0; i < N; i++) { if (Elso == 1980) { Db1980Felett++; } }
+            Console.WriteLine($"2. Feladat: {Db1980Felett} veresenyző lépett 1980-ban pályára először");
+
+            Console.WriteLine(N);
+
+            Console.ReadLine();
+        }
+    }
+}
