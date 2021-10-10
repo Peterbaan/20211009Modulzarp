2e75dae4cf71e727a017e652f14d002552502251
VinczeP <vincze.peter.eger@gmail.com>
2021-10-10 03:36:07 +0200

    Haladunk

--- mzProg2021/Balkezes.cs
+++ mzProg2021/Balkezes.cs
@@ -9,22 +9,24 @@ namespace mzProg2021
     class Balkezes
     {
         public string nev { get; set; }
-        public int elso { get; set; }
-        public int utolso { get; set; }
-        public int suly { get; set; }
-        public int magassag { get; set; }
+        public double elso { get; set; }
+        public double utolso { get; set; }
+        public double suly { get; set; }
+        public double magassag { get; set; }
 
         public Balkezes(string sor)
         {
             string[] s = sor.Split(';');
             nev = s[0];
-            elso = int.Parse(s[1]);
-            utolso = int.Parse(s[2]);
-            suly = int.Parse(s[3]);
-            magassag = int.Parse(s[4]);
+            elso = double.Parse(s[1]);
+            utolso = double.Parse(s[2]);
+            suly = double.Parse(s[3]);
+            magassag = double.Parse(s[4]);
+
         }
         public Balkezes()
         {
+          
         }
     }
 
--- mzProg2021/Program.cs
+++ mzProg2021/Program.cs
@@ -18,16 +18,86 @@ namespace mzProg2021
                 balkezesek.Add(new Balkezes());
             }
 
-            int N = balkezesek.Count;//1. Feladat
-            Console.WriteLine($"{N} db balkezes játékos van.");
+            int N = balkezesek.Count;
+            Console.WriteLine($"{N} db balkezes játékos van.");//1. Feladat
 
-            int Db1980Felett= 0;
-            for (int i = 0; i < N; i++) { if (Elso == 1980) { Db1980Felett++; } }
-            Console.WriteLine($"2. Feladat: {Db1980Felett} veresenyző lépett 1980-ban pályára először");
 
-            Console.WriteLine(N);
+            //int Db1980Felett = 0;
+            //foreach (Balkezes balkezes in balkezesek)
+            //{
+            //    if (balkezes.elso == 1980)
+            //    {
+            //        Db1980Felett++;
+            //    }
+            //}
+            //for (int i = 0; i < N; i++) { if (balkezesek.elso == 1980) { Db1980Felett++; } }
+            //Console.WriteLine($"2. Feladat: {Db1980Felett} veresenyző lépett 1980-ban pályára először"); //2. Feladat
+
+
+
+            double inch = 2.54;
+            double magassagCm = 0;
+            Console.WriteLine("Adjon meg egy nevet!");
+            string nevBekeres = Console.ReadLine();
+
+            //foreach (Balkezes balkezes in balkezesek)
+            //{
+            //    if (balkezes.nev==nevBekeres)
+            //    {
+            //        magassagCm = balkezes.magassag * inch;
+            //        Console.WriteLine($"3. Feladat: A bekért {nevBekeres} nevű játékos magassága: {Math.Round(magassagCm,1)}"); //3.Feladat
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Hibás adat! Próbálja újra!");
+            //        Console.ReadLine();
+            //    }
+            //}
+
+            Console.WriteLine("Adjon meg egy évszámot 1900 és 1999 között!");
+            int Bekeres = int.Parse(Console.ReadLine());
+            //if (1900 <= Bekeres && Bekeres <= 1999)
+            //{
+            //    Console.WriteLine("Helyes adat");
+            //    List<Balkezes> bekertBalkezes = new List<Balkezes>();
+            //    string[] sorok2 = Bekeres == Balkezes.elso;
+            //    foreach (string sor in sorok2.Skip(1))
+            //    {
+            //        bekertBalkezes.Add(new Balkezes());
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hibás adat, adja meg újra!");
+            //    Console.ReadLine(); //4.Feladat
+            //}
+
+            int minIndex = 0;
+            for (int i = 1; i < N; i++)
+            {
+
+                if (balkezesek[i].elso < balkezesek[minIndex].elso)
+                {
+                    minIndex = i;
+                }
+            }
+            Console.WriteLine($"5. Feladat: a legkorábban {balkezesek[minIndex]} évben léptek pályára.");
+
+            int j = 0;
+            while (j < N && balkezesek[j].utolso<2000) { j++; }
+            bool van = j < N;
+            if (van ==true)
+            {
+                Console.WriteLine("6. Feladat: Van, aki 2000 után lépett pályára.");
+            }
+            else
+            {
+                Console.WriteLine("6. Feladat: Nincs aki 2000 után lépett volna pályára.");
+            }
+
 
             Console.ReadLine();
         }
+
     }
 }
